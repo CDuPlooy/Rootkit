cb7b6f58d0405c9eb347e213644e0f6f1f501736
RagingGrim <armand.duplooy@yahoo.com>
2016-05-15 20:57:26 +0000

    Fixed the illegal instruction error.
    Moved the msg variable down ; I remember this from the first time.

--- Rootkit/eros.asm
+++ Rootkit/eros.asm
@@ -1,34 +1,37 @@
 BITS 16
 ORG  0x7C00
-section .data
-msg db "Eros Rootkit",10,"Debug Version",0
+
 section .text
 
-mov eax,msg
-call len
-mov ecx,eax
-mov ebx,msg
+xor eax,eax
+xor ebx,ebx
+xor ecx,ecx
+xor edx,edx
+xor esi,esi
+
+mov esi, msg
 call print
 
 JMP $ ;; HANG
 
-print:
-xor edx,edx
-print_loop:
-mov al ,byte [ebx]
-mov ah , 0x0e
-int 0x10
 
-;;
-;;read for debug
-;;
-add edx,1
-add ebx,1
-cmp ecx,edx
-JE print_done
-JMP print_loop
-print_done:
-ret
+printChar:
+      mov ah,0x0e
+      mov bh,0x0
+      mov bl,0x07
+      int 0x10
+      ret
+
+print:
+      mov al,[si]
+      inc esi
+      or al,al
+      JZ print_done
+      call printChar
+      JMP print
+            print_done:
+            ret
+
 
 len:
       xor ebx , ebx
@@ -39,9 +42,8 @@ len_loop:
       add ebx,1
       JMP len_loop
 len_done:
-      mov eax , ebx
-      xor ebx,ebx
       ret
-
+;;I forgot that the message for some reason needs to be down here. I Never really figured out why.
+msg db "Eros Rootkit" , 0xA , 0x10 , "Debug Version",0
  TIMES 510 - ($ - $$) db 0 ;;zero fills the remaining space
  DW 0xAA55 ;;Signature for the BIOS
