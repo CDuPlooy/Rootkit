748930809474281d9192e066801b8f0a547c60e3
RagingGrim <armand.duplooy@yahoo.com>
2016-05-16 07:43:29 +0200

    Added succ/err functions.

--- Rootkit/eros.asm
+++ Rootkit/eros.asm
@@ -13,8 +13,21 @@ mov esi, msg
 call print
 int 0x11
 
-JMP $ ;; HANG
 
+call hang
+
+err:
+mov esi,errMsg
+call print
+ret
+
+succ:
+mov esi,succMsg
+call print
+ret
+
+hang:
+        JMP $
 
 printChar:
       mov ah,0x0e
@@ -47,5 +60,7 @@ len_done:
 ;;I forgot that the message for some reason needs to be down here. I Never really figured out why.
 ;;AAAAAAH CARRIAGE RETURNS!
 msg db "Eros Rootkit" , 0xA , 0xD , 0x10 , "Debug Version",0x11 ,0xA , 0xD ,0
+errMsg db "An Error Occured",0xA,0xD,0
+succMsg db "Execution Normal",0xA,0xD,0
  TIMES 510 - ($ - $$) db 0 ;;zero fills the remaining space
  DW 0xAA55 ;;Signature for the BIOS
